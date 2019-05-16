3731a9262222f6e5d532e8320f839c016b8ecc8c
Ashok S <ashok.s@target.com>
2019-05-16 16:03:42 +0530

    Set up ManyToMany relationship

---
+++ src/main/java/com/spring/web/domain/Category.java
@@ -0,0 +1,26 @@
+package com.spring.web.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String categoryName;
+	
+	@ManyToMany
+	private Set<Recipe> recipes;
+}
